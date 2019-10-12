1ae447242a9404f8ebd9588813349da60e3bfcf3
Pengzhi Yang <tyypz2590477658@gmail.com>
2019-10-12 15:18:38 +0800

    have a small adjustments on the matlab file

--- FCRN_train/create_underwater.m
+++ FCRN_train/create_underwater.m
@@ -1,6 +1,15 @@
-underwater_rgb1 = exp(-0.25 .* depths(:, :, :)) .* squeeze(double(images(:, :, 1, :)));
-underwater_rgb2 = exp(-0.060 .* depths(:, :, :)) .* squeeze(double(images(:, :, 2, :)));
-underwater_rgb3 = exp(-0.025 .* depths(:, :, :)) .* squeeze(double(images(:, :, 3, :)));
+% Three attenuation parameters which are calculated according to the
+% underwater color chart experiment. Those three parameters could be
+% adjusted according to the real environment.This method is quite 
+% straightforward. Another way is to use waterGan, which is a little 
+% bit harder and inefficient.
+Red_attenuation = -0.25;
+Green_attenuation = -0.060;
+Bule_attenuation = -0.025;
+
+underwater_rgb1 = exp(Red_attenuation .* depths(:, :, :)) .* squeeze(double(images(:, :, 1, :)));
+underwater_rgb2 = exp(Green_attenuation .* depths(:, :, :)) .* squeeze(double(images(:, :, 2, :)));
+underwater_rgb3 = exp(Bule_attenuation .* depths(:, :, :)) .* squeeze(double(images(:, :, 3, :)));
 underwater_rgb = cat(4, underwater_rgb1, underwater_rgb2, underwater_rgb3);
 underwater_rgb = permute(underwater_rgb, [1, 2, 4, 3]);
 underwater_rgb = uint8(underwater_rgb);
